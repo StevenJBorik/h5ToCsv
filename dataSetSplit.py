35968ee627fea08e860d50cd9df686748c1b9005
Steven Borik <SBD2RP@hermanmiller.com>
2023-06-01 15:21:01 -0400

    data split train and testing

--- dataSetSplit.py
+++ dataSetSplit.py
@@ -3,7 +3,7 @@ import os
 from sklearn.model_selection import train_test_split
 
 # Read the dataset from CSV
-df = pd.read_csv('C:\\Users\\SBD2RP\\OneDrive - MillerKnoll\\installs\\Desktop\\songDataSubSetV3.csv')
+df = pd.read_csv('C:\\Users\\SBD2RP\\OneDrive - MillerKnoll\\installs\\Desktop\\songDataSubSetV3Encoded.csv')
 
 # Shuffle the dataset
 df_shuffled = df.sample(frac=1, random_state=42)
@@ -16,5 +16,5 @@ output_directory = 'C:\\Users\\SBD2RP\\OneDrive - MillerKnoll\\installs\\Desktop
 os.makedirs(output_directory, exist_ok=True)
 
 # Save the shuffled and split data into separate CSV files
-train_df.to_csv(os.path.join(output_directory, 'train_data.csv'), index=False)
-test_df.to_csv(os.path.join(output_directory, 'test_data.csv'), index=False)
+train_df.to_csv(os.path.join(output_directory, 'train_data_v1.csv'), index=False)
+test_df.to_csv(os.path.join(output_directory, 'test_data_v1.csv'), index=False)
